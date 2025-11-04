alexbarton/ax-unix

commit d7e3e8389fa7085aa2e6ec13887fcc0f00289c5d

ax-common.sh: Add support for $NO_COLOR
See <https://no-color.org/>.

lib/ax/ax-common.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # ax-common.sh -- Common Functions for Shell Scripts
-# Copyright (c)2013-2018 Alexander Barton (alex@barton.de)
+# Copyright (c)2013-2024 Alexander Barton (alex@barton.de)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ ax_msg1() {
 		"-")	c="1";  shift; ;;	# bold
 		*)	c="0";
 	esac
-	if [ -t 1 ]; then
+	if [ -z "${NO_COLOR}" ] && [ -t 1 ]; then
 		# writing to a terminal, print colored word(s):
 		printf "\\033[0;%sm%s\\033[0m " "${c}" "${1}"
 	else

commit 2d6ab29449c22a54044abac42bf0cd7bd8533585

ax-common.sh: Make script compatible with "set -u"
Commit d7e3e83 ("ax-common.sh: Add support for $NO_COLOR") broke this!

lib/ax/ax-common.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # ax-common.sh -- Common Functions for Shell Scripts
-# Copyright (c)2013-2024 Alexander Barton (alex@barton.de)
+# Copyright (c)2013-2025 Alexander Barton (alex@barton.de)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ ax_msg1() {
 		"-")	c="1";  shift; ;;	# bold
 		*)	c="0";
 	esac
-	if [ -z "${NO_COLOR}" ] && [ -t 1 ]; then
+	if [ -z "${NO_COLOR:-}" ] && [ -t 1 ]; then
 		# writing to a terminal, print colored word(s):
 		printf "\\033[0;%sm%s\\033[0m " "${c}" "${1}"
 	else